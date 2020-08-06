294192d42b25da434e6875fa8a9476737e96075b
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-08-06 16:12:10 +0200

    feat: Add UserMapper interaface

---
+++ src/main/java/pl/uncleglass/cloudstorage/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package pl.uncleglass.cloudstorage.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+import pl.uncleglass.cloudstorage.model.User;
+
+@Mapper
+public interface UserMapper {
+
+    @Select("SELECT * FROM USERS WHERE username = #{username}")
+    User getUser(String username);
+
+    @Insert("INSERT INTO USERS (username, salt, password, firstname, lastname) " +
+            "VALUES (#{username}, #{salt}, #{password}, #{firstName}, #{lastName})")
+    @Options(useGeneratedKeys = true, keyProperty = "userId")
+    int insert(User user);
+}
